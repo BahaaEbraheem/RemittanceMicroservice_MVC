92119b22ddde12d2d6b14e213cd25f413423fad1
Administrator <bahaa.ebraheem812@gmail.com>
2023-01-06 14:54:29 +0200

    Commit

--- modules/currency/host/CurrencyManagment.HttpApi.Host/CurrencyManagmentHttpApiHostModule.cs
+++ modules/currency/host/CurrencyManagment.HttpApi.Host/CurrencyManagmentHttpApiHostModule.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CurrencyManagment.EntityFrameworkCore;
-using CurrencyManagment.MultiTenancy;
+//using CurrencyManagment.MultiTenancy;
 using StackExchange.Redis;
 using Microsoft.OpenApi.Models;
 using Volo.Abp;
--- modules/customer/host/CustomerManagement.HttpApi.Host/CustomerManagementHttpApiHostModule.cs
+++ modules/customer/host/CustomerManagement.HttpApi.Host/CustomerManagementHttpApiHostModule.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CustomerManagement.EntityFrameworkCore;
-using CustomerManagement.MultiTenancy;
+//using CustomerManagement.MultiTenancy;
 using StackExchange.Redis;
 using Microsoft.OpenApi.Models;
 using Volo.Abp;
